143a7b3de5ffacf5a7830798f91db7b109b5759d
val2cak <valentina.tucak99@gmail.com>
2023-12-13 20:43:28 +0100

    adding popup to home

---
+++ pages/components/Popup/Popup.tsx
@@ -0,0 +1,26 @@
+import { IoCloseOutline as CloseIcon } from 'react-icons/io5';
+import brochure from '../../../public/images/brochure.png';
+
+const Popup = ({ onClose }) => {
+  const handleClickOutside = (event) => {
+    if (!event.target.closest('.popup-container')) {
+      onClose();
+    }
+  };
+
+  return (
+    <div
+      className='fixed inset-0 flex items-center justify-center bg-darker bg-opacity-50 z-20'
+      onClick={handleClickOutside}
+    >
+      <div className='text-center popup-container'>
+        <button className='absolute text-light right-5 top-5' onClick={onClose}>
+          <CloseIcon className='text-xl' />
+        </button>
+        <img src={brochure.src} alt='Brochure' className='w-full h-screen' />
+      </div>
+    </div>
+  );
+};
+
+export default Popup;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 
 import Layout from './Layout';
+import Popup from './components/Popup/Popup';
 
 const Home = () => {
   const Cover = dynamic(() => import('./components/Cover/Cover'), {
@@ -19,6 +22,24 @@ const Home = () => {
     ssr: false,
   });
 
+  const [showModal, setShowModal] = useState(false);
+
+  // Check if the 'visited' cookie is set
+  const hasVisited = Cookies.get('visited');
+
+  // Set the 'visited' cookie if it's the first visit
+  useEffect(() => {
+    if (!hasVisited) {
+      // Set the 'visited' cookie to true
+      Cookies.set('visited', 'true', { expires: 7 }); // expires in 7 days
+      setShowModal(true);
+    }
+  }, [hasVisited]);
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <Layout>
       <Cover />
@@ -26,6 +47,8 @@ const Home = () => {
       <Section2 />
       <Section3 />
       <Section4 />
+
+      {showModal && <Popup onClose={closeModal} />}
     </Layout>
   );
 };
