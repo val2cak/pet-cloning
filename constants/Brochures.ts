e6942da71a4122af1f5cc3828bb59f651fddc2b2
val2cak <valentina.tucak99@gmail.com>
2024-01-03 15:02:02 +0100

    changing brochures info

--- constants/Brochures.ts
+++ constants/Brochures.ts
@@ -3,42 +3,42 @@ import { Brochure } from '../types/typeDefinitions';
 export const Brochures: Brochure[] = [
   {
     language: 'en',
-    id: '3R4lcYzG2hRcpwEq02Abkj',
-    code: '9c56be646a91a73687108326b245400a',
+    id: '3eypicvSoVmHOZ3GBJeCZg',
+    code: '0bd309c3d8fc696a036a3055889ce2ee',
   },
   {
     language: 'fr',
-    id: 'gePjbVuZSEGT6MArVxkTT',
-    code: 'd03bc69bf774326c198d806a54b04e7d',
+    id: '2w4sML0v8QYCmyerndSmrf',
+    code: '891d16e3c13ba403d039228e57118500',
   },
   {
     language: 'de',
-    id: '6oAygz374jWDzqjJmvyDOK',
-    code: 'd7e038b717515e0870f365c3ac0689f5',
+    id: '7hpLc6sv4tzXmL3HnZJeMH',
+    code: '6f9646f2f900a484de5a0819ca817a9c',
   },
   {
     language: 'nl',
-    id: '7J0zlOUs6HTpVJeemnbeXB',
-    code: 'e1b83e2177d47b0f589e49f1f3b47c0a',
+    id: '18zBw3OV2qaST1TDYhjqJV',
+    code: '783c1a9e4e9f91bd064b79a725a38a90',
   },
   {
     language: 'es',
-    id: '2yPmwPAcIzfy6G0M9QuaiV',
-    code: '763ebeab593b7bd98187ae75ca376f14',
+    id: '6sUAjcV33hsHpPAFSjADNj',
+    code: '6fb4863e901367867928213d32875b7a',
   },
   {
     language: 'it',
-    id: 'coq5U5WZ5S0xDqH5YORxf',
-    code: '950757dae2d290ec252391c0599914dc',
+    id: 'CHSb5xvPSSR3Zlrvdud7q',
+    code: 'c1f84e1497a43d4cd973fd91bd36c748',
   },
   {
     language: 'hr',
-    id: '6wbIcbicjW8zU00SuuRpbO',
-    code: '31429a6a07d11cd96d116ec96501c9c7',
+    id: '1LzhLSaHVOOIg6YS0yHSAg',
+    code: 'b615983b5ddfa7ce65866c1293cda4c6',
   },
   {
     language: 'ru',
-    id: '233SzFsNFTdM8GRQuxTAdw',
-    code: 'a686ec818ae2131776debe210db085e2',
+    id: '6WLGkkUK7Vc3DK0dJTv3Hf',
+    code: 'd695d0f239c4ade64e924dfdc6335382',
   },
 ];
