14b0d01d0da206a1a1f0009e055085c5fcd561bb
val2cak <valentina.tucak99@gmail.com>
2024-01-17 19:55:27 +0100

    gtm tag 12

--- pages/contact-us/index.tsx
+++ pages/contact-us/index.tsx
@@ -1,4 +1,5 @@
 import dynamic from 'next/dynamic';
+import { GoogleTagManager } from '@next/third-parties/google';
 
 import Layout from '../Layout';
 
@@ -9,6 +10,8 @@ const ContactUs = () => {
 
   return (
     <Layout>
+      <GoogleTagManager gtmId='GTM-5LTK4TCG' />
+
       <div className='sm:h-full h-[95vh] w-full bg-cover sm:bg-contactUsCoverMobile bg-contactUsCover pt-14'>
         <Form />
       </div>
