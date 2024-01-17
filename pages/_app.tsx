28e60b19671fc0325764aa040c918e293c187566
val2cak <valentina.tucak99@gmail.com>
2024-01-17 10:13:02 +0100

    gtm fix 10

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,13 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import Script from 'next/script';
-import { useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
+import { GoogleTagManager } from '@next/third-parties/google';
 
-import * as gtag from '../lib/gtag';
 import { translate } from '../locales/translate';
 
 import '../styles/globals.css';
 
 const App = ({ Component, pageProps }) => {
   const { title } = translate.home.cover;
-
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
 
   return (
     <>
@@ -30,28 +16,11 @@ const App = ({ Component, pageProps }) => {
         <meta name='robots' content='index, follow'></meta>
         <meta name='description' content={title}></meta>
         <link rel='icon' href='./favicon.ico' />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
       </Head>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
-      <Script
-        strategy='afterInteractive'
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
 
       <Toaster position='top-right' />
+      <GoogleTagManager gtmId='GTM-5LTK4TCG' />
+
       <Component {...pageProps} />
     </>
   );
